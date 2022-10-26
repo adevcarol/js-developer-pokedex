7030115a9bc8e9f3de0ea9c2a1a5ae4cc0d0f422
adevcarol <alvscarolina@outlook.com>
2022-10-26 15:15:53 -0300

    Atualização do repositorio com arquivos que criei localmente seguindo as aulas

--- assets/js/poke-api.js
+++ assets/js/poke-api.js
@@ -1,31 +1,28 @@
-
 const pokeApi = {}
 
-function convertPokeApiDetailToPokemon(pokeDetail) {
+function convertPokemonApiDetailToPokemon(pokeDatail){
     const pokemon = new Pokemon()
-    pokemon.number = pokeDetail.id
-    pokemon.name = pokeDetail.name
+    pokemon.number = pokeDatail.id
+    pokemon.name = pokeDatail.name
 
-    const types = pokeDetail.types.map((typeSlot) => typeSlot.type.name)
+    const types = pokeDatail.types.map((typeSlot) => typeSlot.type.name)
     const [type] = types
 
     pokemon.types = types
     pokemon.type = type
 
-    pokemon.photo = pokeDetail.sprites.other.dream_world.front_default
-
+    pokemon.photo = pokeDatail.sprites.other.dream_world.front_default
     return pokemon
 }
 
 pokeApi.getPokemonDetail = (pokemon) => {
     return fetch(pokemon.url)
         .then((response) => response.json())
-        .then(convertPokeApiDetailToPokemon)
+        .then(convertPokemonApiDetailToPokemon)
 }
 
-pokeApi.getPokemons = (offset = 0, limit = 5) => {
+pokeApi.getPokemons = (offset, limit) => {
     const url = `https://pokeapi.co/api/v2/pokemon?offset=${offset}&limit=${limit}`
-
     return fetch(url)
         .then((response) => response.json())
         .then((jsonBody) => jsonBody.results)
